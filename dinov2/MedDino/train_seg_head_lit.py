f0552a11b3341ee5a89ae14b6e825bd5770d5760
kerem-cekmeceli <kerem10016@gmail.com>
2024-03-05 13:21:46 +0100

    m

--- dinov2/MedDino/train_seg_head_lit.py
+++ dinov2/MedDino/train_seg_head_lit.py
@@ -44,14 +44,14 @@ save_checkpoints = True
 log_the_run = True
 
 # Set the BB
-train_backbone = True
+train_backbone = False
 backbone_sz = "small" # in ("small", "base", "large" or "giant")
 
 # Select dataset
 dataset = 'hcp1' # 'hcp2' , cardiac_acdc, cardiac_rvsc, prostate_nci, prostate_usz
 
 # Select the dec head
-dec_head_key = 'unet'  # 'lin', 'fcn', 'psp', 'da', 'resnet', 'unet'
+dec_head_key = 'lin'  # 'lin', 'fcn', 'psp', 'da', 'resnet', 'unet'
 
 # Select loss
 loss_cfg_key = 'ce'  # 'ce', 'dice', 'dice_ce', 'focal', 'focal_dice'
@@ -63,7 +63,7 @@ warmup_iters = 20
 # Config the batch size and lr for training
 batch_sz = 8  # [4, 8, 16, ...]
 lr = 0.5e-4
-weigh_loss_bg = True
+weigh_loss_bg = False
 
 # Test checkpoint
 test_checkpoint_key = 'val_dice'  # 'val_loss', 'val_dice', 'val_mIoU'
