f95d02a119eb8c0f85cefd1f26f9e64bcea3c178
kerem-cekmeceli <kerem10016@gmail.com>
2024-03-24 20:14:18 +0100

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -82,7 +82,7 @@ if model_type == ModelType.SEGMENTOR:
     
 
 # Select dataset
-dataset = 'abide_caltech' # 'hcp1', 'hcp2', abide_caltech, abide_stanford, prostate_nci, prostate_usz, cardiac_acdc, cardiac_rvsc, 
+dataset = 'prostate_nci' # 'hcp1', 'hcp2', abide_caltech, abide_stanford, prostate_nci, prostate_usz, cardiac_acdc, cardiac_rvsc, 
 hdf5_data = True
 
 # Select loss
