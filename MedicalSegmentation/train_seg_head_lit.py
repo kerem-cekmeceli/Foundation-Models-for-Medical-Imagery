4ff161ee1798be5bc8df8b406d58cd633b8f2d78
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-02 15:35:59 +0300

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -61,9 +61,9 @@ from MedicalSegmentation.med_seg_foundation.tools.plot import log_class_rel_freq
 # # No weights - random initialization
 # mdl = get_model(**cfg)
 
-cluster_paths = False
-save_checkpoints = False
-log_the_run = False
+cluster_paths = True
+save_checkpoints = True
+log_the_run = True
 
 # Select model type
 model_type = ModelType.SEGMENTOR
