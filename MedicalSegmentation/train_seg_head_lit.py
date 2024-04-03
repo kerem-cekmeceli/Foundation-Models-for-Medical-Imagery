694fda3acfcd3ae515d3e5eeea5adc58196555b0
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-03 13:30:42 +0300

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -66,7 +66,7 @@ save_checkpoints = True
 log_the_run = True
 
 # Select model type
-model_type = ModelType.SEGMENTOR
+model_type = ModelType.UNET
 
 if model_type == ModelType.SEGMENTOR:
     # Set the BB
