1b16c0f2805b4af3418ddb0d817b986087172480
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-04 01:40:32 +0300

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -83,7 +83,7 @@ if model_type == ModelType.SEGMENTOR:
 # prostate_nci, prostate_usz, 
 # cardiac_acdc, cardiac_rvsc, 
 # spine_mrspinesegv, spine_verse
-dataset = 'prostate_usz' 
+dataset = 'hcp1' 
 rcs_enabled = True
 
 # Select loss
