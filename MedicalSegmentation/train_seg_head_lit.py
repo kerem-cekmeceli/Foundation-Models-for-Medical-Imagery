c80425110bef39f0aa243fe028a5af137a5e3ec3
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-03 14:23:07 +0300

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -66,13 +66,13 @@ save_checkpoints = True
 log_the_run = True
 
 # Select model type
-model_type = ModelType.UNET
+model_type = ModelType.SEGMENTOR
 
 if model_type == ModelType.SEGMENTOR:
     # Set the BB
     backbone = 'dino'  # dino, sam, medsam, resnet
     train_backbone = True
-    backbone_sz = "base" if backbone not in ['sam', 'medsam'] else "base" # in ("small", "base", "large" or "giant")
+    backbone_sz = "small" if backbone not in ['sam', 'medsam'] else "base" # in ("small", "base", "large" or "giant")
     
     # Select the dec head
     dec_head_key = 'unet'  # 'lin', 'fcn', 'psp', 'da', 'segformer', 'resnet', 'unet'
