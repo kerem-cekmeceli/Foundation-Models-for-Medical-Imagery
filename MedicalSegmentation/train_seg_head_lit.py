92562bdd5fe1416dce0fb04e2ca577ad6737a8a4
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-05 21:20:14 +0300

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -66,11 +66,11 @@ save_checkpoints = True
 log_the_run = True
 
 # Select model type
-model_type = ModelType.UNET
+model_type = ModelType.SEGMENTOR
 
 if model_type == ModelType.SEGMENTOR:
     # Set the BB
-    backbone = 'medsam'  # dino, sam, medsam, resnet
+    backbone = 'sam'  # dino, sam, medsam, resnet
     train_backbone = True
     backbone_sz = "small" if backbone not in ['sam', 'medsam'] else "base" # in ("small", "base", "large" or "giant")
     
@@ -83,7 +83,7 @@ if model_type == ModelType.SEGMENTOR:
 # prostate_nci, prostate_usz, 
 # cardiac_acdc, cardiac_rvsc, 
 # spine_mrspinesegv, spine_verse
-dataset = 'hcp2'  
+dataset = 'hcp1'  
 rcs_enabled = True
 
 # Select loss
