32ba6df17e2edee838fe5ddbdf405568b934cb4e
kerem-cekmeceli <kerem10016@gmail.com>
2024-04-13 12:18:05 +0200

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -83,7 +83,7 @@ if model_type == ModelType.SEGMENTOR:
 # prostate_nci, prostate_usz, 
 # cardiac_acdc, cardiac_rvsc, 
 # spine_mrspinesegv, spine_verse
-dataset = 'abide_stanford'  
+dataset = 'abide_caltech'  
 rcs_enabled = True
 
 # Select loss
