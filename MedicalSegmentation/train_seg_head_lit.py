818e3d4f1afeacdab7588279285a91d4635581cd
kerem-cekmeceli <kerem10016@gmail.com>
2024-05-11 14:43:41 +0200

    m

--- MedicalSegmentation/train_seg_head_lit.py
+++ MedicalSegmentation/train_seg_head_lit.py
@@ -72,7 +72,7 @@ if model_type == ModelType.SEGMENTOR:
     # Set the BB
     backbone = 'reinL_sam'  # dino, sam, medsam, resnet, ladderR_, ladderD_, rein_, reinL_
     train_backbone = False and not ('ladder' in backbone or 'rein' in backbone)
-    backbone_sz = "base" if not 'sam' in backbone else "base" # in ("small", "base", "large" or "giant")
+    backbone_sz = "base"  # in ("small", "base", "large" or "giant")
     
     # Select the dec head
         # 'lin', 'fcn', 'psp', 'da', 'segformer', 'resnet', 'unet', 'unetS', 
@@ -86,7 +86,7 @@ if model_type == ModelType.SEGMENTOR:
 # prostate_nci, prostate_usz, 
 # cardiac_acdc, cardiac_rvsc, 
 # spine_mrspinesegv, spine_verse
-dataset = 'prostate_usz'  if cluster_paths else 'hcp1'
+dataset = 'spine_mrspinesegv'  if cluster_paths else 'hcp1'
 rcs_enabled = True
 
 # Select loss
