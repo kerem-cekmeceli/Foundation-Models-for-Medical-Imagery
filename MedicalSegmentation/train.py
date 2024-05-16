88d93e16ea1c87f8f0380909106bb5215f465c04
kerem-cekmeceli <kerem10016@gmail.com>
2024-05-16 12:55:21 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -40,7 +40,7 @@ save_checkpoints = cluster_mode
 log_the_run = cluster_mode
 
 # Select model type
-model_type = ModelType.SEGMENTOR  # SEGMENTOR, UNET, SWINUNET
+model_type = ModelType.UNET  # SEGMENTOR, UNET, SWINUNET
 
 if model_type == ModelType.SEGMENTOR:
     # Set the BB
@@ -59,7 +59,7 @@ if model_type == ModelType.SEGMENTOR:
 # prostate_nci, prostate_usz, 
 # cardiac_acdc, cardiac_rvsc, 
 # spine_mrspinesegv, spine_verse
-dataset = 'spine_verse'  if cluster_paths else 'prostate_usz'
+dataset = 'prostate_usz'  if cluster_paths else 'prostate_usz'
 rcs_enabled = True
 
 # Select loss
