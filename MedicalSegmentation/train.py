7b0be6b2d5b9dadd92d5108805cd85d98f01363c
kerem-cekmeceli <kerem10016@gmail.com>
2024-05-31 13:07:45 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -40,7 +40,7 @@ save_checkpoints = cluster_mode
 log_the_run = cluster_mode
 
 # Select model type
-model_type = ModelType.SEGMENTOR  # SEGMENTOR, UNET, SWINUNET
+model_type = ModelType.SWINUNET  # SEGMENTOR, UNET, SWINUNET
 
 if model_type == ModelType.SEGMENTOR:
     # Set the BB
@@ -85,8 +85,8 @@ pseudo_lab_confidence_thres=0.9
 nb_labeled_vol = 5 if self_training else None
 
 if ftta or self_training:
-    sd_dataset = 'spine_verse'#'prostate_usz'  # To be loaded from saved checkpoints
-    da_dataset = 'spine_mrspinesegv'#'prostate_nci'
+    sd_dataset = 'prostate_usz'#'prostate_usz'  # To be loaded from saved checkpoints
+    da_dataset = 'prostate_nci'#'prostate_nci'
     dataset = da_dataset
     rcs_enabled = False
     
