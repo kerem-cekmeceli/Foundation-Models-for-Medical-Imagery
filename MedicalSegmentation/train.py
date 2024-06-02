b5fd08a79347ad8eeb3d925dc7e26dc483b06029
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-02 20:55:35 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -85,7 +85,7 @@ nb_labeled_vol = 3 if self_training else None
 
 # Domain adaptation
 if ftta or self_training:
-    sd_dataset = 'hcp1'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv
+    sd_dataset = 'hcp2'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv
     da_dataset = 'abide_stanford'#'prostate_nci'
     dataset = da_dataset
     rcs_enabled = False
