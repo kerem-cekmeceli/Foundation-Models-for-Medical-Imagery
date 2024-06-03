bef6ad2349dce1dc7ec8a7c83ce6a787e8a4a3ed
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-04 01:47:42 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -127,7 +127,7 @@ if ftta or self_training:
  
 # Source domain training    
 else:
-    dataset = 'spine_verse'  #if cluster_paths else 'prostate_usz'
+    dataset = 'BraTS_T1'  #if cluster_paths else 'prostate_usz'
     rcs_enabled = True
 
     # Select loss
