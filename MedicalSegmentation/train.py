6eea87fb4dfafae484f5b93b63e991e46d0c59d0
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-04 16:18:32 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -94,8 +94,8 @@ if model_type == ModelType.SEGMENTOR:
 
 # Domain adaptation
 if ftta or self_training:
-    sd_dataset = 'spine_mrspinesegv'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv  
-    da_dataset = 'spine_verse'#'prostate_nci'
+    sd_dataset = 'BraTS_T1'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv  
+    da_dataset = 'BraTS_FLAIR'#'prostate_nci'
     dataset = da_dataset
     rcs_enabled = False
     
