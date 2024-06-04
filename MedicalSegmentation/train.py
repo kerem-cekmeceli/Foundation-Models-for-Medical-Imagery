7dd679200838f233443b342cd1b5e4c1123b978e
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-04 23:48:27 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -83,7 +83,7 @@ if model_type == ModelType.SEGMENTOR:
     # Select the dec head
         # 'lin', 'fcn', 'psp', 'da', 'segformer', 'resnet', 'unet', 'unetS', 
         #'sam_mask_dec', 'hsam_mask_dec', 'hq_sam_mask_dec', 'hq_hsam_mask_dec'
-    dec_head_key = 'hq_hsam_mask_dec' # 'unetS'#'hq_hsam_mask_dec'  
+    dec_head_key = 'unetS' if backbone=='resnet' else  'hq_hsam_mask_dec' # 'unetS'#  'hq_hsam_mask_dec'
 
 # Select dataset
 # 'hcp1', 'hcp2', abide_caltech, abide_stanford, 
@@ -94,8 +94,8 @@ if model_type == ModelType.SEGMENTOR:
 
 # Domain adaptation
 if ftta or self_training:
-    sd_dataset = 'BraTS_T1'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv  
-    da_dataset = 'BraTS_FLAIR'#'prostate_nci'
+    sd_dataset = 'spine_mrspinesegv'#'prostate_usz'  # To be loaded from saved checkpoints  spine_mrspinesegv  
+    da_dataset = 'spine_verse'#'prostate_nci'
     dataset = da_dataset
     rcs_enabled = False
     
