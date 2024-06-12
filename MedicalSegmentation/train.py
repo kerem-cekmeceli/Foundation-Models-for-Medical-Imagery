49133d7b1cad00c7cfb467d67409b3e535f3f08c
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-12 23:39:55 +0200

    m

--- MedicalSegmentation/train.py
+++ MedicalSegmentation/train.py
@@ -53,11 +53,11 @@ nb_labeled_vol = 3 if self_training else None ## 3
 
 if model_type == ModelType.SWINUNET:
     # Set the BB
-    backbone = 'mae'  # dino, dinoReg, sam, medsam, mae, resnet
+    backbone = 'resnet'  # dino, dinoReg, sam, medsam, mae, resnet
     train_backbone = False and not ftta and not self_training
     train_finetune = True and not ftta and not self_training # For reins and reins Lora
     
-    backbone_sz = "large" if cluster_mode else "base" # in ("small", "base", "large" "huge" "giant")
+    backbone_sz = "base" if cluster_mode else "base" # in ("small", "base", "large" "huge" "giant")
     
     # Choose the FineTuning  # ladderR, ladderD, rein, reinL
     if backbone in ['dino', 'dinoReg']:
