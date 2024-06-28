e472c644c7be75082a7a6a5393ff4114c76e5317
kerem-cekmeceli <kerem10016@gmail.com>
2024-06-28 14:31:09 +0200

    updated untracked files

--- MedicalSegmentation/med_seg_foundation/utils/constants.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class ModelType(Enum):
-    SEGMENTOR=1
-    UNET=2
-    SWINUNET=3
-    R2ATTNUNET=4
--- run_slurm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /scratch_net/biwidl210/kcekmeceli/conda/etc/profile.d/conda.sh
-conda activate dinov2
-python -u /scratch_net/biwidl210/kcekmeceli/FoundationModels/MedicalSegmentation/train.py "$@"
